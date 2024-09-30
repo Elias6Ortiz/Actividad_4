705dd7d22a3045b117cc451610350084c2acbd48
Ruben Villalvazo <vim.ruben@hotmail.com>
2024-09-30 19:36:37 -0300

    Refact: Adiciona dark mode y mejora la logica del boton de ir al inicio de la pagina

--- js/scripts.js
+++ js/scripts.js
@@ -10,18 +10,36 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 */
 
-document.getElementById("scrollToTop").onclick = function () {
+// Mostrar el botón cuando el usuario ha hecho scroll más allá del área visible de la pantalla
+window.onscroll = function() {
+  const scrollToTopButton = document.getElementById("scrollToTop");
+  
+  // Mostrar el botón si el scroll es mayor que el área visible de la ventana
+  if (document.documentElement.scrollTop > window.innerHeight) {
+    scrollToTopButton.style.display = "block";
+  } else {
+    scrollToTopButton.style.display = "none";
+  }
+};
+
+// Función para hacer scroll hacia arriba de forma suave
+document.getElementById("scrollToTop").onclick = function() {
   window.scrollTo({
     top: 0,
     behavior: "smooth", // Para un desplazamiento suave
   });
 };
 
+document.getElementById('toggle-dark-mode').addEventListener('click', function() {
+  document.body.classList.toggle('dark-mode');
+});
+
 $(document).ready(function () {
-  $(".carousel").carousel();
   $('.carousel.carousel-slider').carousel({
     fullWidth: true,
     indicators: true
   });
-  $(".dropdown-trigger").dropdown();
+  $(".carousel").carousel({
+    duration: 200,
+  });
 });
